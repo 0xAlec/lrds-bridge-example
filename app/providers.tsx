2fe837b4e599b57f7974e3e401033778d6fbd0a5
Alec Chen <93971719+0xAlec@users.noreply.github.com>
2025-02-14 09:47:52 +0100

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,20 +1,38 @@
 'use client';
 
-import { base } from 'wagmi/chains';
+import { baseSepolia } from 'wagmi/chains';
 import { OnchainKitProvider } from '@coinbase/onchainkit';
 import type { ReactNode } from 'react';
+import { createConfig, http, WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { LRDS_CHAIN } from './chain';
+
+// Add your appchain to the wagmi config
+const wagmiConfig = createConfig({
+  chains: [baseSepolia, LRDS_CHAIN],
+  transports: {
+    [baseSepolia.id]: http(),
+    [LRDS_CHAIN.id]: http(),
+  }
+});
+
+const queryClient = new QueryClient();
 
 export function Providers(props: { children: ReactNode }) {
   return (
     <OnchainKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
-          chain={base}
+          chain={baseSepolia}
           config={{ appearance: { 
             mode: 'auto',
         }
       }}
     >
-      {props.children}
+      <WagmiProvider config={wagmiConfig}>
+        <QueryClientProvider client={queryClient}>
+          {props.children}
+        </QueryClientProvider>
+      </WagmiProvider>
     </OnchainKitProvider>
   );
 }
